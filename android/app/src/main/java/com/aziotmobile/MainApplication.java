0125a7a52304e535d1eebfa8bdef14fd2c90241d
cle5eland <clthomps@microsoft.com>
2019-04-11 11:17:19 -0700

    added app center for ios

--- android/app/src/main/java/com/aziotmobile/MainApplication.java
+++ android/app/src/main/java/com/aziotmobile/MainApplication.java
@@ -3,6 +3,9 @@ package com.aziotmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.reactnative.googlefit.GoogleFitPackage;
 import com.sensors.RNSensorsPackage;
@@ -32,6 +35,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
+            new AppCenterReactNativePackage(MainApplication.this),
             new ReactSliderPackage(),
             new GoogleFitPackage(),
             new RNSensorsPackage(),
