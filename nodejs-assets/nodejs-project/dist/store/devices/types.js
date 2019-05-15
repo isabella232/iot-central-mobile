2fa83b63b09b5149469e810baf4f42c657c0b338
cle5eland <clthomps@microsoft.com>
2019-05-15 10:14:09 -0700

    fixed android release build

--- nodejs-assets/nodejs-project/dist/store/devices/types.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// action types
-exports.CONNECT_DEVICE = "CONNECT_DEVICE";
--- nodejs-assets/nodejs-project/dist/store/telemetry/actions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var types_1 = require("./types");
-// action creators
-function updateTelemetry(telemetry) {
-    return { type: types_1.UPDATE_TELEMETRY, telemetry: telemetry };
-}
-exports.updateTelemetry = updateTelemetry;
